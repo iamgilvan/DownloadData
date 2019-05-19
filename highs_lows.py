2246a150dc1678cb304fb3310ec65bb551585fa6
Gilvan Almeida <g6almeida@hotmail.com>
2019-05-19 00:17:46 -0300

    Tratando os erros.

--- highs_lows.py
+++ highs_lows.py
@@ -2,7 +2,7 @@ import csv
 from matplotlib import pyplot as plt
 from datetime import datetime
 
-file_name = './sitka_weather_2014.csv'
+file_name = './death_valley_2014.csv'
 
 with open(file_name) as f:
     reader = csv.reader(f)
@@ -10,12 +10,16 @@ with open(file_name) as f:
 
     dates, highs, lows = [], [], []
     for row in reader:
-
-        dates.append(datetime.strptime(row[0], "%Y-%m-%d"))
-
-        highs.append(int(row[1]))
-
-        lows.append(int(row[3]))
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 
 
@@ -25,7 +29,8 @@ fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, c='red', alpha=0.5)
 plt.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
-plt.title("Daily high and low temperatures - 2014", fontsize=24)
+title = "Daily high and low temperatures - 2014\nDeath Valley, CA"
+plt.title(title, fontsize=24)
 plt.xlabel('', fontsize=16)
 
 fig.autofmt_xdate()
