a90d7013e935187663c5f333f0ea64fccf490998
Gilvan Almeida <g6almeida@hotmail.com>
2019-06-02 23:30:20 -0300

    Lendo dados do json.

---
+++ world_population.py
@@ -0,0 +1,14 @@
+import json
+
+# carrega dados em uma lista
+filename = 'population_data.json'
+
+with open(filename) as f:
+    pop_data = json.load(f)
+
+# Exibe a população de cada país em 2010
+for pop_dict in pop_data:
+    if pop_dict['Year'] == '2010':
+        country_name = pop_dict['Country Name']
+        population = int(float(pop_dict['Value']))
+        print(country_name + ": " + str(population))
